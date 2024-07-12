73ba40f5cb6a77b6b09901f18f0a963f07423c97
Mynul Islam <mynul.sakil@gmail.com>
2024-07-12 19:45:09 +0600

    add serach function

--- shoping-guru-client/src/Components/ProductList/ProductList.jsx
+++ shoping-guru-client/src/Components/ProductList/ProductList.jsx
@@ -7,7 +7,9 @@ import toast from "react-hot-toast";
 
 const ProductList = ({ addToCart }) => {
   const [products, setProducts] = useState([]);
-
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  // for handle api call
   useEffect(() => {
     axios
       .get("http://localhost:8000/")
@@ -20,6 +22,17 @@ const ProductList = ({ addToCart }) => {
         console.log(err);
       });
   }, []);
+  // for search
+  useEffect(() => {
+    const newProduct = products.filter((product) => {
+      if (search === "") {
+        return product;
+      } else if (product.name.toLowerCase().includes(search.toLowerCase())) {
+        return product;
+      }
+    });
+    setSearchResult(newProduct);
+  }, [search]);
 
   const addProduct = (product) => {
     setProducts([...products, product]);
@@ -29,12 +42,35 @@ const ProductList = ({ addToCart }) => {
   return (
     <div className="container mx-auto p-4 grid grid-cols-3 gap-5">
       <div className="col-span-2">
-        <h1 className="text-2xl font-bold mb-4">Product List</h1>
+        <div>
+          <h1 className="text-2xl font-bold mb-4">Product List</h1>
+          {/* search input */}
+          <div className="relative shadow-lg rounded mb-4 my-4 px-4">
+            <input
+              type="text"
+              placeholder="Search product"
+              className="border p-2 mb-2 w-full"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            {/* cross button for search clear*/}
+            <button
+              onClick={() => setSearch("")}
+              className="bg-red-500 text-white px-2  rounded absolute right-0 top-0 mt-2 mr-2"
+            >
+              Clear
+            </button>
+          </div>
+        </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {products.map((product, index) => (
+        {/* if search==="" show ganeral product if not show search product */}
+          {search===""?  products.map((product, index) => (
             <Product key={index} product={product} addToCart={addToCart} />
-          ))}
+          )):
+          searchResult.map((product, index) => (
+            <Product key={index} product={product} addToCart={addToCart} />
+          ))
+          }
         </div>
       </div>
       <div className="py-20 h-full w-full bg-gray-50 px-4 rounded-lg">
